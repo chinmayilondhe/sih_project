f623cf3eb601b1ce481527ad3ccde813c344d0ec
chinmayilondhe <chinmayilondhe73@gmail.com>
2022-03-21 23:37:52 +0530

    changes in login screen

--- lib/screens/loginpage.dart
+++ lib/screens/loginpage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sih_project/screens/userlogin.dart';
 
@@ -12,27 +13,89 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            color: Colors.lightBlue.shade100
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("LOGIN",style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),),
-              SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){
+      body: SingleChildScrollView(
+        child: Container(
 
-              }, child: Text("Admin")),
-              ElevatedButton(onPressed: (){
-               Navigator.of(context).push(MaterialPageRoute(builder: (c)=>UserLogin()));
-              }, child: Text("user")),
+          // padding: EdgeInsets.all(16),
+          // decoration: BoxDecoration(
+          //     color: Colors.lightBlue.shade100
+          // ),
+          child:
+             Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Stack(
+              children:[
+                  Container(
+                  height: 200,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                      // topLeft: Radius.elliptical(200, 100),
+                          bottomRight:  Radius.elliptical(150,60),
+                          // bottomLeft:  Radius.circular(140)
+                      ),
+                    gradient: new LinearGradient(
+                        colors: [Colors.lightBlueAccent.withOpacity(0.6), Colors.cyanAccent.withOpacity(0.2)],
+                        begin: Alignment.centerRight,
+                        end: Alignment.centerLeft
+                    ),
+                  ),
+                  ),
+                   Positioned(
+                     top: 120,
+                     left: MediaQuery.of(context).size.width/3.5,
+                     child: Text(""
+                        "Welcome",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,color: Colors.blueAccent.shade400),),
+                   ),
+                ]),
 
-            ],
+                ClipRRect(
+                    child: Image.asset("images/sih_img.png",height: 300,)),
+                SizedBox(height: 30,),
+
+                SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(26)),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(7),
+                          backgroundColor:
+                          MaterialStateProperty.all(Colors.cyanAccent.shade700.withOpacity(0.4)),
+                        ),
+                        onPressed: () {},
+                        child: const Text("Admin",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+                  ),
+                ),
+                SizedBox(height: 40,),
+                SizedBox(
+                  width: 150,
+                  height: 50,
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(26)),
+                    child: ElevatedButton(
+                        style: ButtonStyle(
+                          elevation: MaterialStateProperty.all(7),
+                          backgroundColor:
+                          MaterialStateProperty.all(Colors.cyanAccent.shade700.withOpacity(0.6)),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (c)=>UserLogin()));
+                        },
+                        child: const Text("User",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold))),
+                  ),
+                ),
+                SizedBox(height: 20,),
+
+
+              ],
+            ),
           ),
-        ),
       ),
+      
     );
   }
 }
