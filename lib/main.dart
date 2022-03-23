c17fd9dd0ba898a78fc2aae4355b5ed537be7470
Atharva14641 <atharva.deore20@vit.edu>
2022-03-23 23:23:45 +0530

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sih_project/screens/loginpage.dart';
+import 'dart:async';
+import 'package:flutter/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +16,57 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreen createState() => _SplashScreen();
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginPage())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.all(15),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("images/image1.jpg"),
+          Text(
+            '\nCare App',
+            style: TextStyle(
+              color: Color.fromRGBO(39, 105, 171, 1),
+              fontSize: MediaQuery.of(context).size.width / 12,
+              decoration: TextDecoration.none,
+              fontFamily: 'Nunito',
+            ),
+          ),
+          Text(
+            '\n\n\n\n\n\n\nTech Savy Youth',
+            style: TextStyle(
+              color: Color.fromRGBO(39, 105, 171, 1),
+              fontSize: MediaQuery.of(context).size.width / 20,
+              decoration: TextDecoration.none,
+              fontFamily: 'Nunito',
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
